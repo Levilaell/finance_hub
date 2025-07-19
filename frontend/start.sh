645ccfa48e39cd5c1b5f9f35489976e475abd7bb
Levi Lael <levilael2@hotmail.com>
2025-07-19 02:12:40 -0300

    try

--- frontend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Start script for Next.js standalone server
-
-# Use PORT environment variable, default to 3000 if not set
-export PORT=${PORT:-3000}
-
-# Start the Next.js server
-exec node server.js
